b2574694f3d6b34c1d45b2f77cdff2bb7194e968
r <eng.raghdaa@gmail.com>
2025-09-06 02:59:45 +0300

    cleanup

--- kit/op.py
+++ kit/op.py
@@ -12,8 +12,6 @@ import torchac
 # Select device globally
 if torch.cuda.is_available():
     device = torch.device('cuda')
-# elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
-#     device = torch.device('mps')
 else:
     device = torch.device('cpu')
 
